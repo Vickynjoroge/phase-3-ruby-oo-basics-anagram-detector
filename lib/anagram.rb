bd9e7a4a38330f52a99b77e0bf6a1c41141c07a9
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-05-19 12:30:03 +0300

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        sorted_word = @word.chars.sort.join
+        array.filter {|w|w.chars.sort.join == sorted_word}
+    end
+end
+
